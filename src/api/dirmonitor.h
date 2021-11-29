62c41085ddc01942c36ec5b000f802ee26a352f9
Adam Harrison <adamdharrison@gmail.com>
2021-11-28 19:45:21 -0500

    Removed unecessary file.

--- src/api/dirmonitor.h
+++
@@ -1,9 +0,0 @@
-#ifndef DIRMONITOR_H
-#define DIRMONITOR_H
-
-struct dirmonitor* init_dirmonitor();
-void deinit_dirmonitor(struct dirmonitor* monitor);
-int check_dirmonitor(struct dirmonitor* monitor, int (*change_callback)(int, void*), void* data);
-int add_dirmonitor(struct dirmonitor* monitor, const char* path);
-
-#endif
