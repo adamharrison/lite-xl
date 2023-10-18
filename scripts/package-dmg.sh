f9bd48063ec0ff45909ecf58a2ead3c1572c203f
Adam Harrison <adamdharrison@gmail.com>
2023-10-18 13:38:39 -0400

    Made things recursive.

--- scripts/package-dmg.sh
+++ scripts/package-dmg.sh
@@ -7,9 +7,10 @@ if [ ! -e "src/api/api.h" ]; then
 fi
 
 mkdir -p "Lite XL.app/Contents/MacOS" "Lite XL.app/Contents/Resources"
-cp $1/lite-xl/lite-xl "Lite XL.app/Contents/MacOS"
-cp $1/lite-xl/data/* "Lite XL.app/Contents/Resources"
-cp $1/lite-xl/Info.plist "Lite XL.app/Contents/Resources"
+cp -r $1/lite-xl/lite-xl "Lite XL.app/Contents/MacOS"
+cp -r $1/lite-xl/data/* "Lite XL.app/Contents/Resources"
+cp resources/icons/icon.icns "Lite XL.app/Contents/Resources"
+cp -r $1/Info.plist "Lite XL.app/Contents/Resources"
 
 cat > lite-xl-dmg.json << EOF
 {
