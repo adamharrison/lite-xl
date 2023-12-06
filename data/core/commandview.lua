52d70374ab07ae9954374d7f3c90128303f1a3f0
Adam Harrison <adamdharrison@gmail.com>
2023-12-06 08:58:10 -0500

    Virtual lines begins.
    Initial commit of virtual lines PR.
    
    Making it so that linewrapping also works.
    
    Things proceed apace.
    
    We proceed apace.
    
    Fixed many things.
    
    Fixed a number of bugs.
    
    Standardizing naming conventions.
    
    Fixed issue with resolving screen position on multi-lines.
    
    Added in read-only mode.
    
    Fixed selections.
    
    Gloriously beautiful.
    
    Uncommented modified selcetions.
    
    Rearranged things to be more abstracted.
    
    Fixed line length issue.
    
    Rearchitected token storage.
    
    Fixed invalidations.
    
    Hooked up highlighter to syntax highlighting.
    
    Added in rqeuest to tokenize extra lines.
    
    Reduced size.
    
    Bumped modversion.
    
    Fixed issue with cache not resizing appropriately.
    
    Changed over resolve_screen_position to new system.
    
    Mixed up order of parameters.
    
    Small little bug fixes.
    
    Ensure we don't wrap over.
    
    Removed dev statement.
    
    More small fixes.
    
    Fixed some codefolding and autocomplete issues.
    
    Fixed up plugins that still use doc.

--- data/core/commandview.lua
+++ data/core/commandview.lua
@@ -111,7 +111,7 @@ end
 function CommandView:set_text(text, select)
   self.last_text = text
   self.doc:remove(1, 1, math.huge, math.huge)
-  self.doc:text_input(text)
+  self:text_input(text)
   if select then
     self.doc:set_selection(math.huge, math.huge, 1, 1)
   end
@@ -301,7 +301,7 @@ function CommandView:update()
       if #self.last_text < #current_text and
          string.find(suggested_text, current_text, 1, true) == 1 then
         self:set_text(suggested_text)
-        self.doc:set_selection(1, #current_text + 1, 1, math.huge)
+        self:set_selection(1, #current_text + 1, 1, math.huge)
       end
       self.last_text = current_text
     end
