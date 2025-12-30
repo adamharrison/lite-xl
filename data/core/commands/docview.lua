1b56fe6902234e3684e9fd30f504030e71d6da0b
Adam Harrison <adamdharrison@gmail.com>
2025-12-30 11:52:27 -0500

    Fixed moved lines up and moved lines down for multicursors.

--- data/core/commands/docview.lua
+++ data/core/commands/docview.lua
@@ -108,6 +108,23 @@ local function insert_paste(dv, value, whole_line, idx)
   end
 end
 
+local function merged_line_blocks(dv)
+  local sels = {}
+  for idx, line1, col1, line2, col2 in doc_multiline_selections(dv, true) do
+    if #sels > 0 and sels[#sels][2] >= line1 - 1 then
+      sels[#sels][2] = line2
+    else
+      table.insert(sels, { line1, line2 })
+    end
+  end
+  local i = 0
+  return function()
+    i = i + 1
+    if i > #sels then return nil end
+    return i, sels[i][1], sels[i][2]
+  end
+end
+
 local write_commands = {
   ["docview:cut"] = function(dv)
     cut_or_copy(dv, true)
@@ -282,26 +299,38 @@ local write_commands = {
   end,
 
   ["docview:move-lines-up"] = function(dv)
+    local sels = {}
     for idx, line1, col1, line2, col2 in doc_multiline_selections(dv, true) do
+      table.insert(sels, { line1 - 1, col1, line2 - 1, col2 })
+    end
+    for idx, line1, line2 in merged_line_blocks(dv, true) do
       append_line_if_last_line(dv, line2)
       if line1 > 1 then
         local text = dv.doc.lines[line1 - 1]
         dv.doc:insert(line2 + 1, 1, text)
         dv.doc:remove(line1 - 1, 1, line1, 1)
-        dv:set_selections(idx, line1 - 1, col1, line2 - 1, col2)
       end
+    end
+    for idx, sel in ipairs(sels) do
+      dv:set_selections(idx, table.unpack(sel))
     end
   end,
 
   ["docview:move-lines-down"] = function(dv)
+    local sels = {}
     for idx, line1, col1, line2, col2 in doc_multiline_selections(dv, true) do
+      table.insert(sels, { line1 + 1, col1, line2 + 1, col2 })
+    end
+    for idx, line1, line2 in merged_line_blocks(dv, true) do
       append_line_if_last_line(dv, line2 + 1)
       if line2 < #dv.doc.lines then
         local text = dv.doc.lines[line2 + 1]
         dv.doc:remove(line2 + 1, 1, line2 + 2, 1)
         dv.doc:insert(line1, 1, text)
-        dv:set_selections(idx, line1 + 1, col1, line2 + 1, col2)
       end
+    end
+    for idx, sel in ipairs(sels) do
+      dv:set_selections(idx, table.unpack(sel))
     end
   end,
 
